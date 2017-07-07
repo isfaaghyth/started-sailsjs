06c453184d587e1e5102be95ac3e94342299339c
Muh.Isfahani Ghiyath.YM <isfaaghyth@users.noreply.github.com>
2017-07-07 19:33:59 +0700

    create post model and controller

---
+++ api/models/Posts.js
@@ -0,0 +1,26 @@
+/**
+ * Posts.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+   attributes: {
+     id: {
+         type: 'integer',
+         primaryKey: true,
+         autoIncrement: true
+     },
+     title: {
+         type: 'string'
+     },
+     slug: {
+         type: 'string'
+     },
+     content: {
+         type: 'string'
+     }
+   }
+};
